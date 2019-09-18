5ca55ca79a0f62fd1f4099b296649f88d4bb859c
Brichet <brichet.b@hotmail.fr>
2019-09-18 19:11:28 +0200

    Changement de MethodGame: - Obliger l'utilisateur à saisir 4 chiffres ( problème cas où l'utilisateur commence par 0 sa saisie.) - Problème sur la saisie, l'utilisateur doit saisir 2 fois sa combinaison.

--- src/com/ocr/escapegame/ChallengerMode.java
+++ src/com/ocr/escapegame/ChallengerMode.java
@@ -11,6 +11,8 @@ public class ChallengerMode {
      * Déroulement du mode Challenger
      */
     public void runChallengerMode() {
+        int nP = 0;
+
         System.out.println("Vous avez choisi comme mode de jeu : Challenger");
         System.out.println("");
         System.out.println("Principe du jeu en mode Challenger :");
@@ -39,35 +41,46 @@ public class ChallengerMode {
 
 
          // Proposition de l'utilisateur
-        int nP = 0;
+        boolean Winner = false;
         do {
             if (nP == 0) {
                 System.out.println("Donner votre première proposition ");
-                System.out.println("");
             }
             else if (nP == 1) {
                 System.out.println("Donner votre deuxième proposition ");
-                System.out.println("");
             }
             else if (nP == 2) {
-                System.out.println("Donner votre deuxième proposition ");
-                System.out.println("");
+                System.out.println("Donner votre troisième proposition ");
             }
             else if (nP == 3) {
-                System.out.println("Donner votre troisième proposition ");
-                System.out.println("");
-            }
-            else if (nP == 4) {
                 System.out.println("Donner votre quatrième et dernière proposition ");
-                System.out.println("");
             }
 
+            // Confirmer la bonne saisie de l'utilisateur et la récupérer
             MethodGame.saisirCombinaison();
+            int[] userCombinaison = MethodGame.saisirCombinaison();
 
-            int userCombinaison = MethodGame.saisirCombinaison();
-                MethodGame.comparerCombinaison(userCombinaison);
+            // Faire la comparaison des deux combinaisons et retourner le résultat.
+                String[] ReponseFinal = MethodGame.comparerCombinaison(userCombinaison,IaCombinaison);
+                System.out.println("");
+                System.out.println("La réponse pour le 1er chiffre est "+ReponseFinal[0]);
+                System.out.println("La réponse pour le 2ème chiffre est "+ReponseFinal[1]);
+                System.out.println("La réponse pour le 3ème chiffre est "+ReponseFinal[2]);
+                System.out.println("La réponse pour le 4ème chiffre est "+ReponseFinal[3]);
+                System.out.println("");
+
+            // Si toutes les combinaisons sont égales à "=" alors la partie est gagné sinon retenté sa chance
+            if (ReponseFinal[0] == "=" && ReponseFinal[1] == "=" && ReponseFinal[2] == "=" && ReponseFinal[3] == "="){
+                Winner = true;
+                System.out.println("Bien joué");
+            }else {
                 nP++;
-            // TODO Comparaison du chiffre avec celui de l'Ia
-        } while (nP <= 4);// nP n'est pas égale à 4 ou que la réponse à été trouvée
+            }
+        } while (nP <= 3 || Winner == true);// nP n'est pas égale à 4 ou que la réponse à été trouvée
+            if (Winner){
+                System.out.println(" Félicitation, vous avez trouvé la combinaison !!!");
+            }else {
+                System.out.println(" Dommage... Vous n'avez pas réussi à trouver la combinaison.");
+            }
     }
 }
--- src/com/ocr/escapegame/MethodGame.java
+++ src/com/ocr/escapegame/MethodGame.java
@@ -1,42 +1,80 @@
 package com.ocr.escapegame;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MethodGame {
     /**
      * Saisie d'un nombre obligatoire
+     * @return
      */
     // TODO Erreur, à voir, il demande 2 fois minimum la saisie de la combinaison par l'utilisateur
-        public static int saisirCombinaison() {
-            Scanner sc = new Scanner(System.in);
-            int userCombinaison = 0;
-            boolean Ucombi;
-            do {
-                System.out.println("DOoDOo");
+    public static int[] saisirCombinaison() {
+        Scanner sc = new Scanner(System.in);
+        boolean Ucombi = false;
+        int [] userCombinaison = new int [4];
+        int p1,p2,p3,p4,pt,pU = 0;
+        String Verify = null;
+        do {
                 try {
                     //Saisie de l'utilisateur
-                    int pU = sc.nextInt();
-                    if (pU < 999 || pU > 10000) {
+                    pU = sc.nextInt();
+
+                Verify = String.valueOf(pU);
+
+            if (Verify.length() >= 5 || Verify.length() <=3){
+                System.out.println("Vous n'avez pas saisie une combinaison à 4 chiffres");
+                Ucombi =false;
+            }else {
+
+                p1 = pU / 1000;
+                pt = pU - (p1 * 1000);
+                p2 = pt / 100;
+                pt = pt - (p2 * 100);
+                p3 = pt / 10;
+                p4 = pt - (p3 * 10);
+
+                userCombinaison[0] = p1;
+                userCombinaison[1] = p2;
+                userCombinaison[2] = p3;
+                userCombinaison[3] = p4;
+
+                Ucombi = true;
+            }
+                } catch (InputMismatchException e) {
                         System.out.println("Vous n'avez pas saisie une combinaison à 4 chiffres");
+                        sc.next();
                         Ucombi = false;
-                    } else {
-                        System.out.println("Vous avez bien saisie une combinaison à 4 chiffres");
-                        Ucombi = true;
-                        userCombinaison = pU;
                     }
-                }catch (InputMismatchException e){
-                    System.out.println("Vous n'avez pas saisie une combinaison à 4 chiffres");
-                    sc.next();
-                    Ucombi =false;
-                }
-                System.out.println(Ucombi);
-            }while (!Ucombi);
-            return userCombinaison;
-        }
-
-        public static char comparerCombinaison(int userCombinaison){
-
-            return 0;
+            System.out.println(Ucombi);
+        }while (!Ucombi);
+        return userCombinaison;
+    }
+    /**
+     * Comparaison des deux combinaisons
+     * @param userCombinaison combinaison de l'utilisateur
+     * @param iaCombinaison combinaison de l'ia
+     * @return la réponse à l'utilisateur "-=-+"
+     */
+        public static String[] comparerCombinaison(int[] userCombinaison, int[] iaCombinaison){
+                String combinaisonString = String.valueOf(userCombinaison);
+                //char [] chCombinaison = new char[4];
+                char [] userReponse = new char[4];
+                String [] reponse = new String[4];
+                    for ( int m =0; m <4 ; m++){
+                        //chCombinaison[m] = combinaisonString.charAt(m);
+                        //int ChiffreComparaison = Integer.parseInt(String.valueOf(chCombinaison[m]));
+                        if (iaCombinaison[m] == userCombinaison[m]) {
+                            userReponse[m] = '=';
+                        } else if (iaCombinaison[m] < userCombinaison[m]){
+                            userReponse [m] = '-';
+                        }
+                        else if (iaCombinaison[m] > userCombinaison[m]){
+                            userReponse [m] = '+';
+                        }
+                        reponse [m] = Character.toString(userReponse[m]);
+                    }
+            return reponse;
         }
 }
